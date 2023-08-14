c360b6c8e90e56cd5388d8b3112b553d9455a097
Michael Chambers <guess_what10@hotmail.com>
2023-08-14 16:22:07 -0700

    basic logic working

--- scanner.py
+++ scanner.py
@@ -5,28 +5,42 @@ from dns import reversename
 
 LOG = logging.getLogger("ip-scanner")
 stdout_handler = logging.StreamHandler(sys.stdout)
-stdout_handler.setLevel(logging.debug)
+stdout_handler.setLevel(logging.DEBUG)
 stdout_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 stdout_handler.setFormatter(stdout_formatter)
 LOG.addHandler(stdout_handler)
 
-IP_ADDRESS = "45.60.121.229"
+IP_ADDRESS = "127.0.0.1"
+IIS_VERSION = "7.0"
+NGINX_VERSION = "1.2"
 
-url = reversename.from_address(IP_ADDRESS)
-LOG.debug("Identified URL is %s" % url)
-response = requests.get(url)
 
-if response.status_code == 200 and '<title>Index of /</title>' in response.text:
-    print('Directory listing is enabled')
-else:
-    print('Directory listing is not enabled')
+def scanner(ip):
+    result = {
+        "ip_address": ip,
+        "server": "N/A",
+        "version": "N/A",
+        "directory": False
+    }
+    response = requests.get("http://" + ip)
 
-if "server" in response.headers:
-    server = response.headers["server"]
-    print(server)
-    if "nginx" in server.lower():
-        print("The server is running Nginx.")
-    elif "iis" in server.lower():
-        print("The server is running IIS.")
-else:
-    print("The server type could not be determined.")
+    # directory = bool
+    if response.status_code == 200 and '<title>Index of /</title>' in response.text:
+        result.update({"directory": True})
+
+    if "server" in response.headers:
+        server_string = response.headers["server"]
+        try:
+            server, version = server_string.split("/")
+            if version:
+                result.update({"server": server})
+                result.update({"version": version})
+        except ValueError:
+            result.update({"server": server_string})
+    else:
+        result.append(ip, "N/A", "N/A", sep='\t')
+    return result
+
+
+print("IP Address", "Server", "Version", "Directory")
+print(scanner(IP_ADDRESS))
