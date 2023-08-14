39e48fd7043c37a58256b9fe3170e6bd56ff2283
Michael Chambers <guess_what10@outlook.com>
2023-08-14 11:47:36 -0700

    some progress

--- scanner.py
+++ scanner.py
@@ -1,8 +1,25 @@
 import requests
+import logging
+import sys
+from dns import reversename
 
-url = "https://www.digicert.com"
+LOG = logging.getLogger("ip-scanner")
+stdout_handler = logging.StreamHandler(sys.stdout)
+stdout_handler.setLevel(logging.debug)
+stdout_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+stdout_handler.setFormatter(stdout_formatter)
+LOG.addHandler(stdout_handler)
 
+IP_ADDRESS = "45.60.121.229"
+
+url = reversename.from_address(IP_ADDRESS)
+LOG.debug("Identified URL is %s" % url)
 response = requests.get(url)
+
+if response.status_code == 200 and '<title>Index of /</title>' in response.text:
+    print('Directory listing is enabled')
+else:
+    print('Directory listing is not enabled')
 
 if "server" in response.headers:
     server = response.headers["server"]
